663142c3e0f9eb4f65916376947dd24d4df29de9
Callum <callumdavidrossiter@gmail.com>
2020-04-04 15:18:32 +0100

    TODOs

--- connection.php
+++ connection.php
@@ -2,6 +2,9 @@
 function getConnection(){
     //$db = new PDO("mysql:host=localhost;dbname=unn_w15021554",'unn_w15021554','SXIK5W1D');
     $db = new PDO("mysql:host=localhost;dbname=welcomeu",'root','');
+
+    //$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
  return $db;
 
 }

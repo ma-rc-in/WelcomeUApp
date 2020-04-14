a0b39140def915905c7be5764c8f7b709c86d3ab
Callum <callumdavidrossiter@gmail.com>
2020-04-14 14:53:08 +0100

    Help Services - Chatbot

---
+++ help.js
@@ -0,0 +1,21 @@
+function chat() {
+    var chat = document.getElementById("hhChatInput").value;
+    var chat = chat.toLowerCase()
+    var chatOutput = document.getElementById("hmChatOutput");
+
+    //reset password
+    if(chat.indexOf('change') != -1 || chat.indexOf('reset') != -1
+        && chat.indexOf('password') != -1) {
+        chatOutput.value = "You can change the password by contacting ";
+    }
+
+    else{
+        chatOutput.value = "I do not know what you are trying to ask, please try again.";
+    }
+
+
+    //<a href='https://libraryanswers.northumbria.ac.uk/index.php'>here</a>
+
+    //if more than 2 trys, try click here
+    // error
+}

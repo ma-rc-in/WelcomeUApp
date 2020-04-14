137f1f83512dc5a0f5e6904b51ea0b89cfeaa708
madihaahmed <61740924+madihaahmed@users.noreply.github.com>
2020-04-14 14:28:23 +0100

    English Version

--- settings.js
+++ settings.js
@@ -6,7 +6,6 @@ function setLanguage() {
     }
 }
 
-
 function languageChange() {
     var language = localStorage.getItem("language");
     if (language == null){//checks to see if the user has a preference set, if not
@@ -17,65 +16,122 @@ function languageChange() {
 
     //gets the IDs of all menu areas
     //main Menu
-        var mapMenu= document.getElementById("mapMenu");
-        var groupChatMenu = document.getElementById("groupChatMenu");
-        var helpMenu = document.getElementById("helpMenu");
-        var annoucementsMenu= document.getElementById("annoucementsMenu");
-        var enrolmentMenu = document.getElementById("enrolmentMenu");
-        var settingsMenu= document.getElementById("settingsMenu");
+    var mapMenu= document.getElementById("mapMenu");
+    var groupChatMenu = document.getElementById("groupChatMenu");
+    var helpMenu = document.getElementById("helpMenu");
+    var annoucementsMenu= document.getElementById("annoucementsMenu");
+    var enrolmentMenu = document.getElementById("enrolmentMenu");
+    var settingsMenu= document.getElementById("settingsMenu");
 
     //login
-        var llWelcomeU = document.getElementById("llWelcomeU");
-        var llLoginMessage  = document.getElementById("llLoginMessage");
-        var llStudentID = document.getElementById("llStudentID");
-        var llPassword = document.getElementById("llPassword");
-        var llsubmit= document.getElementById("llSubmit");
+    var llWelcomeU = document.getElementById("llWelcomeU");
+    var llLoginMessage  = document.getElementById("llLoginMessage");
+    var llStudentID = document.getElementById("llStudentID");
+    var llPassword = document.getElementById("llPassword");
+    var llsubmit= document.getElementById("llSubmit");
 
     //groupChat
-        var courseNameTitleGroupChat = document.getElementById("gcCourseNameTitle");
-        var yourMessageGroupChat = document.getElementById("gcYourMessage");
-        var MessageContentBoxGroupChat = document.getElementById("gcMessageContentBox");
-        var reportButtonGroupChat = document.getElementById("gcReportButton");
-        var sendButtonGroupChat = document.getElementById("gcSendButton");
-        var reportUserGroupChat = document.getElementById("gcReportUser");
-        var userIDGroupChat = document.getElementById("gcUserID");
-        var reasonLabelGroupChat = document.getElementById("gcReasonTitle");
-        var reasonGroupChat = document.getElementById("gcReason");
-        var reasonSelectGroupChat = document.getElementById("reportType");
-        var commentGroupChat = document.getElementById("reportComment");
-        var reportSubmitGroupChat = document.getElementById("gcReportSubmit");
+    var courseNameTitleGroupChat = document.getElementById("gcCourseNameTitle");
+    var yourMessageGroupChat = document.getElementById("gcYourMessage");
+    var MessageContentBoxGroupChat = document.getElementById("gcMessageContentBox");
+    var reportButtonGroupChat = document.getElementById("gcReportButton");
+    var sendButtonGroupChat = document.getElementById("gcSendButton");
+    var reportUserGroupChat = document.getElementById("gcReportUser");
+    var userIDGroupChat = document.getElementById("gcUserID");
+    var reasonLabelGroupChat = document.getElementById("gcReasonTitle");
+    var reasonGroupChat = document.getElementById("gcReason");
+    var reasonSelectGroupChat = document.getElementById("reportType");
+    var commentGroupChat = document.getElementById("reportComment");
+    var reportSubmitGroupChat = document.getElementById("gcReportSubmit");
 
     //self enrolment
-        //selfEnrolmentForm
-        var eeTitleOne= document.getElementById("eeTitleOne");
-        var eeStepOne = document.getElementById("eeStepOne");
-        var eePersonalDetails = document.getElementById("eePersonalDetails");
-        var eeTitle= document.getElementById("eeTitle");
-        var eeFirstName = document.getElementById("eeFirstName");
-        var eeLastName= document.getElementById("eeLastName");
-        var eeGender= document.getElementById("eeGender");
-        var eePersonalEmail = document.getElementById("eePersonalEmail");
-        var eeukMobile = document.getElementById("eeukMobile");
-        var eeukMobile2 = document.getElementById("eeukMobile2");
-        var eeEmergenceTitle = document.getElementById("eeEmergenceTitle");
-        var eeEmergencyPerson= document.getElementById("eeEmergencyPerson");
-        var eeEmergencyRelationship = document.getElementById("eeEmergencyRelationship");
-        var country= document.getElementById("country");
-        var eeContactNo = document.getElementById("eeContactNo");
-        var eesubmit= document.getElementById("submit");
-        //uploadPhoto
-        var eeTitleTwo = document.getElementById("eeTitleTwo");
-        var eeStepTwo= document.getElementById("eeStepTwo");
-        var eeSmartCardCurrent = document.getElementById("eeSmartCardCurrent");
-        var eeSmartCardUpload= document.getElementById("eeSmartCardUpload");
-        var eeFilename = document.getElementById("eeFilename");
-        var eesubmitTwo= document.getElementById("submit");
-        //selfEnrolmentCompleted
-        var eeSuccess = document.getElementById("eeSuccess");
-        var eeComplete = document.getElementById("eeComplete");
-        var eeLink = document.getElementById("eeLink");
+    //selfEnrolmentForm
+    var eeTitleOne= document.getElementById("eeTitleOne");
+    var eeStepOne = document.getElementById("eeStepOne");
+    var eePersonalDetails = document.getElementById("eePersonalDetails");
+    var eeTitle= document.getElementById("eeTitle");
+    var eeFirstName = document.getElementById("eeFirstName");
+    var eeLastName= document.getElementById("eeLastName");
+    var eeGender= document.getElementById("eeGender");
+    var eePersonalEmail = document.getElementById("eePersonalEmail");
+    var eeukMobile = document.getElementById("eeukMobile");
+    var eeukMobile2 = document.getElementById("eeukMobile2");
+    var eeEmergenceTitle = document.getElementById("eeEmergenceTitle");
+    var eeEmergencyPerson= document.getElementById("eeEmergencyPerson");
+    var eeEmergencyRelationship = document.getElementById("eeEmergencyRelationship");
+    var country= document.getElementById("country");
+    var eeContactNo = document.getElementById("eeContactNo");
+    var eesubmit= document.getElementById("submit");
+    //uploadPhoto
+    var eeTitleTwo = document.getElementById("eeTitleTwo");
+    var eeStepTwo= document.getElementById("eeStepTwo");
+    var eeSmartCardCurrent = document.getElementById("eeSmartCardCurrent");
+    var eeSmartCardUpload= document.getElementById("eeSmartCardUpload");
+    var eeFilename = document.getElementById("eeFilename");
+    var eesubmitTwo= document.getElementById("submit");
+    //selfEnrolmentCompleted
+    var eeSuccess = document.getElementById("eeSuccess");
+    var eeComplete = document.getElementById("eeComplete");
+    var eeLink = document.getElementById("eeLink");
 
+    //settings
+    var ssTextSecurity = document.getElementById("ssTextSecurity");
+    var ssButtonSecurity = document.getElementById("ssButtonSecurity");
+    var ssTextNotifications = document.getElementById("ssTextNotifications");
+    var ssFirstBtn = document.getElementById("ssFirstBtn");
+    var ssTextLanguage = document.getElementById("ssTextLanguage");
+    var ssSecondBtn = document.getElementById("ssSecondBtn");
+    var gcUserID = document.getElementById("gcUserID");
+    var ssTextModalNotifications = document.getElementById("ssTextModalNotifications");
+    var ssTextTheme = document.getElementById("ssTextTheme");
+    var ssThirdBtn = document.getElementById("ssThirdBtn");
+    var ssThemeDark = document.getElementById("ssThemeDark");
+    var ssThemeLight = document.getElementById("ssThemeLight");
+    var ssTextContrast = document.getElementById("ssTextContrast");
+    var ssTextContrastModal = document.getElementById("ssTextContrastModal");
+    var ssTextContrast = document.getElementById("ssTextContrast");
+    var ssFourthBtn = document.getElementById("ssFourthBtn");
 
+    //userDashboard
+    //userDashboard
+    var udTextHeaderPass = document.getElementById("udTextHeaderPass");
+    var udButtonTextFirstBtn = document.getElementById("udButtonTextFirstBtn");
+    var udTextHeaderAccount = document.getElementById("udTextHeaderAccount");
+    var udButtonTextSecondBtn = document.getElementById("udButtonTextSecondBtn");
+    var udTextHeaderPin = document.getElementById("udTextHeaderPin");
+    var udButtonTextThirdBtn = document.getElementById("udButtonTextThirdBtn");
+    var udTextModalOldPassPlaceholder = document.getElementById("udTextModalOldPassPlaceholder");
+    var udTextModalNewPassPlaceholder = document.getElementById("udTextModalNewPassPlaceholder");
+    var udTextModalRepeatNewPassPlaceholder = document.getElementById("udTextModalRepeatNewPassPlaceholder");
+    var udButtonTextSubmitButtonForPass = document.getElementById("udButtonTextSubmitButtonForPass");
+    var udModalTextHeaderWarningNote = document.getElementById("udModalTextHeaderWarningNote");
+    var udModalTextBodyWarningNote = document.getElementById("udModalTextBodyWarningNote");
+    var udTextModalRepeatPassDeleteAccountPlaceholder = document.getElementById("udTextModalRepeatPassDeleteAccountPlaceholder");
+    var udButtonTextSubmitButtonForDeleteAccount = document.getElementById("udButtonTextSubmitButtonForDeleteAccount");
+    var udTextModalNewPinPlaceholder = document.getElementById("udTextModalNewPinPlaceholder");
+    var udTextModalRepeatPinPlaceholder = document.getElementById("udTextModalRepeatPinPlaceholder");
+    var udButtonTextSubmitButtonForChangePin = document.getElementById("udButtonTextSubmitButtonForChangePin");
+    //userDashboardAdmin
+    var udTextHeaderPass = document.getElementById("udTextHeaderPass");
+    var udButtonTextFirstBtn = document.getElementById("udButtonTextFirstBtn");
+    var udTextHeaderAccount = document.getElementById("udTextHeaderAccount");
+    var udButtonTextSecondBtn = document.getElementById("udButtonTextSecondBtn");
+    var udTextHeaderPin = document.getElementById("udTextHeaderPin");
+    var udButtonTextThirdBtn = document.getElementById("udButtonTextThirdBtn");
+    var udTextModalOldPassPlaceholder = document.getElementById("udTextModalOldPassPlaceholder");
+    var udTextModalNewPassPlaceholder = document.getElementById("udTextModalNewPassPlaceholder");
+    var udTextModalRepeatNewPassPlaceholder = document.getElementById("udTextModalRepeatNewPassPlaceholder");
+    var udButtonTextSubmitButtonForPass = document.getElementById("udButtonTextSubmitButtonForPass");
+    var udModalTextHeaderWarningNote = document.getElementById("udModalTextHeaderWarningNote");
+    var udModalTextBodyWarningNote = document.getElementById("udModalTextBodyWarningNote");
+    var udTextModalRepeatPassDeleteAccountPlaceholder = document.getElementById("udTextModalRepeatPassDeleteAccountPlaceholder");
+    var udButtonTextSubmitButtonForDeleteAccount = document.getElementById("udButtonTextSubmitButtonForDeleteAccount");
+    var udTextModalNewPinPlaceholder = document.getElementById("udTextModalNewPinPlaceholder");
+    var udTextModalRepeatPinPlaceholder = document.getElementById("udTextModalRepeatPinPlaceholder");
+    var udButtonTextSubmitButtonForChangePin = document.getElementById("udButtonTextSubmitButtonForChangePin");
+
+    //Map
+    var mpSearch = document.getElementById("pac-input");
     //English
     if (language == "English"){ //each page needs a try catch so that it can load without the other pages elements
         //Main Menu
@@ -153,6 +209,56 @@ function languageChange() {
             eeLink.innerHTML = "Please click this <a style=\"color: #3498DB\" href=\"https://www.northumbria.ac.uk/study-at-northumbria/new-students/\">link</a> to view your programme induction timetable";
         }catch (e) {}
 
-    }
+        //settings
+        try {
+            ssTextSecurity.innerHTML = "Security settings";
+            ssButtonSecurity.innerHTML = "Change";
+            ssTextNotifications.innerHTML = "Notifications settings";
+            ssFirstBtn.innerHTML = "notifications settings";
+            ssTextLanguage.innerHTML = "Change language";
+            ssSecondBtn.innerHTML = "language";
+            ssTextModalNotifications.innerHTML = "Enable notification alert sound?";
+            ssTextTheme.innerHTML = "Change theme";
+            ssThirdBtn.innerHTML = "Change theme";
+            ssThemeDark.innerHTML = "Dark theme";
+            ssThemeLight.innerHTML = "Light theme";
+            ssTextContrast.innerHTML = "High Contrast";
+            ssTextContrastModal.innerHTML = "Enable high contrast setting?";
+            ssTextContrast.innerHTML = "Change contrast";
+            ssFourthBtn.value = "Change";
+        } catch (e) {}
 
+        //userDashboard
+        try {
+            udTextHeaderPass.innerHTML = "Change password";
+            udButtonTextFirstBtn.innerHTML = "Change";
+            udTextHeaderAccount.innerHTML = "Delete account";
+            udButtonTextSecondBtn.innerHTML = "Delete";
+            udTextHeaderPin.innerHTML = "Set PIN";
+            udButtonTextThirdBtn.innerHTML = "Set";
+
+            //ERROR MESSAGE ID TRANSLATION????
+
+            udTextModalOldPassPlaceholder.placeholder = "Enter your current password";
+            udTextModalNewPassPlaceholder.placeholder = "Enter your new password";
+            udTextModalRepeatNewPassPlaceholder.placeholder = "Repeat your new password";
+            udButtonTextSubmitButtonForPass.innerHTML = "Submit";
+
+            udModalTextHeaderWarningNote.innerHTML = "WARNING";
+            udModalTextBodyWarningNote.innerHTML = "You are about to delete all data related to your account, so you will no longer be able to use this application. If you want to continue, please enter your current password below and click on 'submit'.";
+            udTextModalRepeatPassDeleteAccountPlaceholder.placeholder = "Enter your password to delete your account";
+            udButtonTextSubmitButtonForDeleteAccount.innerHTML = "Submit";
+
+            udTextModalNewPinPlaceholder.placeholder = "Enter your new PIN";
+            udTextModalRepeatPinPlaceholder.placeholder = "Repeat your new PIN";
+            udButtonTextSubmitButtonForChangePin.innerHTML = "Submit";
+        } catch (e) {}
+
+        //map
+        try {
+            mpSearch.placeholder = "Type Northumbria...";
+        } catch (e) {}
+
+
+    }
 }

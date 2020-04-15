512ba2a822b38dbeec3964772c8aea918f4425ba
madihaahmed <61740924+madihaahmed@users.noreply.github.com>
2020-04-15 19:22:41 +0100

    announcement changes

--- announcementsLecturer.php
+++ announcementsLecturer.php
@@ -67,9 +67,9 @@ else
 
         /* Style the submit button with a specific background color etc */
         input[type=submit] {
-          background-color: #4CAF50;
+          background-color: #9e9e9e;
           color: white;
-          padding: 12px 20px;
+          padding: 12px 120px;
           border: none;
           border-radius: 4px;
           cursor: pointer;
@@ -77,7 +77,7 @@ else
 
         /* When moving the mouse over the submit button, add a darker green color */
         input[type=submit]:hover {
-          background-color: #45a049;
+          background-color: #9e9e9e;
         }
 
         /* Add a background color and some padding around the form */
@@ -93,7 +93,7 @@ else
   <form action="announcementsLecturer.php">
 
     <label for="fname">Module Code</label>
-      <?php
+      //<?php
     <select id="courseID ='$CourseID'";
             $studentInfo = getStudentDetails(); //gets all student details
       $ID = $studentInfo['studentID'];

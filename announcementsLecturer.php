b2d89629913183616e091d4b80702df475226c7f
Callum <callumdavidrossiter@gmail.com>
2020-04-15 19:23:09 +0100

    Announcements Lecturer- Test Fixed.

--- announcementsLecturer.php
+++ announcementsLecturer.php
@@ -5,9 +5,18 @@ $db = getConnection();//returns the connection for the database.
 
 session_start();
 
+function convertModuleID($ID){ //this converts the module ID into a name
+    $db = getConnection();
+    $modulequery = "select moduleName from tbl_module where moduleID='$ID'";
+    $module = $db->query($modulequery);
+    $row = $module->fetch(PDO::FETCH_ASSOC);
+    $moduleName = $row['moduleName'];
+    return $moduleName;
+}
+
 if (checkAccessType() != "Lecturer") {
-        header('Location:announcmentsStudent.php');
-    }
+    header('Location:announcmentsStudent.php');
+}
 else
 
 ?>
@@ -52,94 +61,92 @@ else
                 display: table;
             }
         }
-        
-                /* Style inputs with type="text", select elements and textareas */
+
+        /* Style inputs with type="text", select elements and textareas */
         input[type=text], select, textarea {
-          width: 100%; /* Full width */
-          padding: 12px; /* Some padding */  
-          border: 1px solid #ccc; /* Gray border */
-          border-radius: 4px; /* Rounded borders */
-          box-sizing: border-box; /* Make sure that padding and width stays in place */
-          margin-top: 6px; /* Add a top margin */
-          margin-bottom: 16px; /* Bottom margin */
-          resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
+            width: 100%; /* Full width */
+            padding: 12px; /* Some padding */
+            border: 1px solid #ccc; /* Gray border */
+            border-radius: 4px; /* Rounded borders */
+            box-sizing: border-box; /* Make sure that padding and width stays in place */
+            margin-top: 6px; /* Add a top margin */
+            margin-bottom: 16px; /* Bottom margin */
+            resize: vertical /* Allow the user to vertically resize the textarea (not horizontally) */
         }
 
         /* Style the submit button with a specific background color etc */
         input[type=submit] {
-          background-color: #4CAF50;
-          color: white;
-          padding: 12px 20px;
-          border: none;
-          border-radius: 4px;
-          cursor: pointer;
+            background-color: #4CAF50;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
         }
 
         /* When moving the mouse over the submit button, add a darker green color */
         input[type=submit]:hover {
-          background-color: #45a049;
+            background-color: #45a049;
         }
 
         /* Add a background color and some padding around the form */
         .container {
-          border-radius: 5px;
-          background-color: #f2f2f2;
-          padding: 20px;
+            border-radius: 5px;
+            background-color: #f2f2f2;
+            padding: 20px;
         }
     </style>
 </head>
 <body>
 <div class="container">
-  <form action="action_page.php">
+    <form action="action_page.php">
 
-    <label for="fname">Module Code</label>
-    <select id="courseID ='$CourseID'";
+        <label for="fname">Module Code</label>
+
+        <select id="selectModule" name="selectModule">
+            <?php
             $studentInfo = getStudentDetails(); //gets all student details
-      $ID = $studentInfo['studentID'];
+            $ID = $studentInfo['studentID'];
 
-      //gets the course ID of the student
-      $studentidquery = "select courseID from tbl_student where studentID='$ID'";
-      $student = $db->query($studentidquery);
-      $row = $student->fetch(PDO::FETCH_ASSOC);
-      $CourseID = $row['courseID'];
-                $course = $db->query($coursequery);
-                while ($row = $course->fetchObject()) {
-                    $module1ID = $row->module1;
-                    $module1 = convertModuleID($module1ID);
-                    echo '<option value="' . $module1ID . '">' . $module1ID . " - " . $module1 . '</option>';
-                    $module2ID = $row->module2;
-                    $module2 = convertModuleID($module2ID);
-                    echo '<option value="' . $module2ID . '">' . $module2ID . " - " . $module2 . '</option>';
-                    $module3ID = $row->module3;
-                    $module3 = convertModuleID($module3ID);
-                    echo '<option value="' . $module3ID . '">' . $module3ID . " - " . $module3 . '</option>';
-                    $module4ID = $row->module4;
-                    $module4 = convertModuleID($module4ID);
-                    echo '<option value="' . $module4ID . '">' . $module4ID . " - " . $module4 . '</option>';
-                    $module5ID = $row->module5;
-                    $module5 = convertModuleID($module5ID);
-                    echo '<option value="' . $module5ID . '">' . $module5ID . " - " . $module5 . '</option>';
-                    $module6ID = $row->module6;
-                    $module6 = convertModuleID($module6ID);
-                    echo '<option value="' . $module6ID . '">' . $module6ID . " - " . $module6 . '</option>';
-                    $module7ID = $row->module7;
-                    $module7 = convertModuleID($module7ID);
-                    echo '<option value="' . $module7ID . '">' . $module7ID . " - " . $module7 . '</option>';
-                    $module8ID = $row->module8;
-                    $module8 = convertModuleID($module8ID);
-                    echo '<option value="' . $module7ID . '">' . $module7ID . " - " . $module7 . '</option>';
-                }
-    </select>
+            //gets the course ID of the student
+            $studentidquery = "select courseID from tbl_student where studentID='$ID'";
+            $student = $db->query($studentidquery);
+            $row = $student->fetch(PDO::FETCH_ASSOC);
+            $CourseID = $row['courseID'];
 
-    <label for="subject"> Subject </label>
-    <input type="text" id="subject" name="Subject" placeholder="Type a subject..">
+            $coursequery = "select * from tbl_course where courseID='$CourseID'";
+            $course = $db->query($coursequery);
+            while ($row = $course->fetchObject())
+            {
+                $module1ID = $row->module1; $module1 = convertModuleID($module1ID);
+                echo '<option value="'.$module1ID.'">'.$module1ID." - ".$module1.'</option>';
+                $module2ID = $row->module2; $module2 = convertModuleID($module2ID);
+                echo '<option value="'.$module2ID.'">'.$module2ID." - ".$module2.'</option>';
+                $module3ID = $row->module3; $module3 = convertModuleID($module3ID);
+                echo '<option value="'.$module3ID.'">'.$module3ID." - ".$module3.'</option>';
+                $module4ID = $row->module4; $module4 = convertModuleID($module4ID);
+                echo '<option value="'.$module4ID.'">'.$module4ID." - ".$module4.'</option>';
+                $module5ID = $row->module5; $module5 = convertModuleID($module5ID);
+                echo '<option value="'.$module5ID.'">'.$module5ID." - ".$module5.'</option>';
+                $module6ID = $row->module6; $module6 = convertModuleID($module6ID);
+                echo '<option value="'.$module6ID.'">'.$module6ID." - ".$module6.'</option>';
+                $module7ID = $row->module7; $module7 = convertModuleID($module7ID);
+                echo '<option value="'.$module7ID.'">'.$module7ID." - ".$module7.'</option>';
+                $module8ID = $row->module8; $module8 = convertModuleID($module8ID);
+                echo '<option value="'.$module7ID.'">'.$module7ID." - ".$module7.'</option>';
+            }
+            ?>
+        </select>
 
-    <label for="message">Message</label>
-    <textarea id="message" name="message" placeholder="Type your message.." style="height:200px"></textarea>
+        <label for="subject"> Subject </label>
+        <input type="text" id="subject" name="Subject" placeholder="Type a subject..">
 
-    <input type="submit" value="Submit">
+        <label for="message">Message</label>
+        <textarea id="message" name="message" placeholder="Type your message.." style="height:200px"></textarea>
 
-  </form>
+        <input type="submit" value="Submit">
+
+    </form>
 </div>
 
 <script>
